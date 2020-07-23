af29d9bb01cb25caf6cd9412e4f63a237424737b
André 'Dezzy <andrevcv1@gmail.com>
2020-07-23 18:47:58 -0300

    chore: Set page load strategy to 'normal'

--- src/utils/webdriver.ts
+++ src/utils/webdriver.ts
@@ -9,17 +9,20 @@ Chrome.setDefaultService(new Chrome.ServiceBuilder(ChromeDriver.path).build());
 const DEFAULT_OPTIONS = new Chrome.Options();
 DEFAULT_OPTIONS.addArguments('disable-infobars');
 DEFAULT_OPTIONS.setUserPreferences({ credential_enable_service: false });
+DEFAULT_OPTIONS.setPageLoadStrategy('normal');
 
 export const build = (
   options: Chrome.Options = DEFAULT_OPTIONS,
-): ThenableWebDriver => new Builder().setChromeOptions(options).forBrowser('chrome').build();
+): ThenableWebDriver =>
+  new Builder().setChromeOptions(options).forBrowser('chrome').build();
 
 export const quit = async (webdriver: WebDriver): Promise<void> => {
   console.log(
     `Quitting browser in ${process.env.BROWSER_QUIT_SECONDS_TIMEOUT} seconds...`,
   );
 
-  const timeoutMilliseconds = (Number(process.env.BROWSER_QUIT_SECONDS_TIMEOUT) * 1000) / 2;
+  const timeoutMilliseconds =
+    (Number(process.env.BROWSER_QUIT_SECONDS_TIMEOUT) * 1000) / 2;
 
   await sleep(timeoutMilliseconds);
   await webdriver.quit();
